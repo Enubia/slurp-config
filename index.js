959e53d0c29b7bc820b700154ad718404c4517fd
Marco Schuster <marco.schuster@avafin.com>
2023-06-27 16:19:00 +0200

    fix: formatting

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
 module.exports = {
     root: true,
-    parser: "vue-eslint-parser",
+    parser: 'vue-eslint-parser',
     parserOptions: {
-        parser: "@typescript-eslint/parser",
-        sourceType: "module"
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
     },
     extends: [
-        "eslint:recommended",
-        "plugin:vue/vue3-recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "prettier"
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'prettier'
     ],
     plugins: [
-        "prettier",
-        "@typescript-eslint",
-        "unicorn",
-        "import",
-        "unused-imports"
+        'prettier',
+        '@typescript-eslint',
+        'unicorn',
+        'import',
+        'unused-imports'
     ],
     rules: {
-        'prettier/prettier': "error",
+        'prettier/prettier': 'error',
         curly: [
-            "error",
-            "all"
+            'error',
+            'all'
         ],
-        'dot-notation': "error",
+        'dot-notation': 'error',
         'no-console': [
-            "warn",
+            'warn',
             {
                 allow: [
-                    "warn",
-                    "error"
+                    'warn',
+                    'error'
                 ]
             }
         ],
-        'no-debugger': "error",
-        'no-lonely-if': "error",
-        'no-param-reassign': "warn",
+        'no-debugger': 'error',
+        'no-lonely-if': 'error',
+        'no-param-reassign': 'warn',
         'no-restricted-syntax': [
-            "error",
-            "ForInStatement",
-            "LabeledStatement",
-            "WithStatement"
+            'error',
+            'ForInStatement',
+            'LabeledStatement',
+            'WithStatement'
         ],
-        'no-throw-literal': "error",
-        'no-unused-vars': "off",
-        'no-useless-constructor': "off",
-        'no-useless-rename': "error",
-        'object-shorthand': "error",
-        'prefer-object-spread': "error",
-        'require-atomic-updates': "error",
+        'no-throw-literal': 'error',
+        'no-unused-vars': 'off',
+        'no-useless-constructor': 'off',
+        'no-useless-rename': 'error',
+        'object-shorthand': 'error',
+        'prefer-object-spread': 'error',
+        'require-atomic-updates': 'error',
 
-        '@typescript-eslint/no-inferrable-types': "error",
+        '@typescript-eslint/no-inferrable-types': 'error',
         '@typescript-eslint/no-unused-vars': [
-            "error",
+            'error',
             {
-                argsIgnorePattern: "^_"
+                argsIgnorePattern: '^_'
             }
         ],
-        '@typescript-eslint/no-useless-constructor': "error",
+        '@typescript-eslint/no-useless-constructor': 'error',
 
         'import/extensions': [
-            "error",
-            "ignorePackages",
+            'error',
+            'ignorePackages',
             {
-                js: "never",
-                mjs: "never",
-                jsx: "never",
-                ts: "never",
-                tsx: "never"
+                js: 'never',
+                mjs: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never'
             }
         ],
-        'import/first': "error",
-        'import/no-mutable-exports': "error",
+        'import/first': 'error',
+        'import/no-mutable-exports': 'error',
         'import/no-unresolved': [
             2,
             {
@@ -80,29 +80,29 @@ module.exports = {
             }
         ],
         'import/order': [
-            "error",
+            'error',
             {
-                'newlines-between': "always",
+                'newlines-between': 'always',
                 alphabetize: {
-                    order: "asc",
+                    order: 'asc',
                     caseInsensitive: true
                 }
             }
         ],
 
-        'unicorn/error-message': "error",
-        'unicorn/escape-case': "error",
-        'unicorn/no-instanceof-array': "error",
-        'unicorn/no-new-buffer': "error",
-        'unicorn/number-literal-case': "error",
-        'unicorn/prefer-includes': "error",
-        'unicorn/prefer-string-starts-ends-with': "error",
-        'unicorn/prefer-type-error': "error",
-        'unicorn/throw-new-error': "error",
+        'unicorn/error-message': 'error',
+        'unicorn/escape-case': 'error',
+        'unicorn/no-instanceof-array': 'error',
+        'unicorn/no-new-buffer': 'error',
+        'unicorn/number-literal-case': 'error',
+        'unicorn/prefer-includes': 'error',
+        'unicorn/prefer-string-starts-ends-with': 'error',
+        'unicorn/prefer-type-error': 'error',
+        'unicorn/throw-new-error': 'error',
 
-        'unused-imports/no-unused-imports': "error",
+        'unused-imports/no-unused-imports': 'error',
 
-        'vue/multi-word-component-names': "off"
+        'vue/multi-word-component-names': 'off'
     },
     env: {
         jest: false,
@@ -113,20 +113,20 @@ module.exports = {
     settings: {
         'import/parsers': {
             '@typescript-eslint/parser': [
-                ".ts"
+                '.ts'
             ]
         },
         'import/resolver': {
             node: {
                 extensions: [
-                    ".js",
-                    ".ts"
+                    '.js',
+                    '.ts'
                 ]
             }
         },
         'import/ignore': [
-            "node_modules",
-            "\\.(coffee|scss|css|less|hbs|svg|json)$"
+            'node_modules',
+            '\\.(coffee|scss|css|less|hbs|svg|json)$'
         ]
     }
 }
